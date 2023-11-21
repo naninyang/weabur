c753a373970e2b78732f0a04b606df1dbfb9cdc8
Chloe <gtalk.with.nani@gmail.com>
2023-11-21 20:28:01 +0900

    Settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withImages = require("next-images");
+// const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  // pwa: {
+  //   dest: "public",
+  //   runtimeCaching: require("next-pwa/cache"),
+  // },
+};
 
-module.exports = nextConfig
+// module.exports = withImages(withPWA(nextConfig));
+module.exports = withImages(nextConfig);
