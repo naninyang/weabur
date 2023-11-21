04408f3fa0fb59b90016f4a957a1496dd101efc0
Chloe <gtalk.with.nani@gmail.com>
2023-11-21 21:03:29 +0900

    Container styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import styles from '@/styles/home.module.sass';
 
 export default function Home() {
   return (
-    <main className={styles.home}>
-      <p>웨버</p>
+    <main className={styles.weabur}>
+      <div className={styles.container}>내용</div>
     </main>
   );
 }
