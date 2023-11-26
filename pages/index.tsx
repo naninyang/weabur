2e883cd0e2e52b06a6cc963f98946429026e9d40
Chloe <gtalk.with.nani@gmail.com>
2023-11-26 19:03:18 +0900

    Responsive UX

--- pages/index.tsx
+++ pages/index.tsx
@@ -499,6 +499,7 @@ export default function Home() {
                   </ul>
                 </PerfectScrollbar>
               )}
+              <div className={styles.dummy} />
             </div>
           </>
         )}
