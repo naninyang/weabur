e5b67555443e76bbff5a08a4a7c2306903bf2588
Chloe <gtalk.with.nani@gmail.com>
2023-11-28 15:29:01 +0900

    Favicon link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link color="#141414" href="/favicon/safari-pinned-tab.svg" rel="mask-icon" />
+        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon/favicon.ico" rel="shortcut icon" />
+        <link href="/manifest.json" rel="manifest" />
+        <meta name="application-name" content="웨버 WeaBur" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="Black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="웨버 WeaBur" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#212121" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#212121" />
         <link
           rel="apple-touch-startup-image"
           media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
