542b0b477b89567a94c421dc885f6f49dbc78bc7
Chloe <gtalk.with.nani@gmail.com>
2023-12-15 21:42:36 +0900

    Change gtag location

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { GA_TRACKING_ID } from '@/utils/gtag';
+import { GA_TRACKING_ID } from '@/lib/gtag';
 
 export default function Document() {
   return (
